22c1dce4f3af6c356a8c398a3ba7ae0e6c65bc31
Cristian Quispe Ramirez <cquispe@belatrix.com>
2018-04-19 09:57:09 -0500

    Resolve fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function isNullOrEmpty (input) {
-    return (input === undefined || input === null || input === '');
+    return (input === undefined || input === null || input === '' || input.trim().length === 0);
 }
 
 module.exports = isNullOrEmpty;
