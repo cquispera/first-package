d22d2f6edd726b2f8e2df6a97476c24734b93c0d
Cristian Quispe Ramirez <cquispe@belatrix.com>
2018-04-19 10:25:53 -0500

    Beta vesion with allowEmptyVaules conditional

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function isNullOrEmpty (input) {
-    return (input === undefined || input === null || input === '' || input.trim().length === 0);
+function isNullOrEmpty (input, allowEmptyValues) {
+    if (allowEmptyValues){
+        return (input === undefined || input === null || input === '');
+    }else {
+        return (input === undefined || input === null || input === '' || input.trim().length === 0);
+    }
+    
 }
 
 module.exports = isNullOrEmpty;
